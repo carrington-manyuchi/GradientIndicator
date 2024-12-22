e01c10553099d18a3321f225beeca6bd90fa2388
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-12-22 11:42:58 +0200

    Initial Commit

---
+++ GradientIndicator/GradientIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientIndicatorApp.swift
+//  GradientIndicator
+//
+//  Created by Manyuchi, Carrington C on 2024/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GradientIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
